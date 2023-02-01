f612b84d6c0723b55944ffdd01301538bd1a7e5f
Молодец 2705 <ant_papa@mail.ru>
2023-02-01 15:13:49 +0600

    propsproblem

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,38 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {BrowserRouter, Route} from "react-router-dom";
+import DialogItem from "./components/Dialogs/DialogItem/DialogItem";
+
+/*type DialogItemType = {
+    name: string
+    id: number
+}*/
 
 
-const App = () => {
+
+
+const App = (props: any) => {
+
+    let dialogs = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+    ]
+
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/profile' component={Profile}/>
-                    <Route path='/news' component={News}/>
-                    <Route path='/music' component={Music}/>
-                    <Route path='/settings' component={Settings}/>
+                    <Route path='/dialogs' render={ () => <Dialogs id={props.id} name={props.name}/> }/>
+                    <Route path='/profile' render={ () => <Profile />}/>
+                    <Route path='/news' render={ () => <News />}/>
+                    <Route path='/music' render={ () => <Music />}/>
+                    <Route path='/settings' render={ () => <Settings />}/>
                 </div>
             </div>
         </BrowserRouter>
