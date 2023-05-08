d14a41d8a032982baa9721864569fa90a4d153d6
Молодец 2705 <ant_papa@mail.ru>
2023-05-08 12:01:53 +0600

    message-reducer

--- src/redax/message-reducer.tsx
+++ src/redax/message-reducer.tsx
@@ -1,7 +1,22 @@
 import {AllType} from "../App";
+import {PostType} from "../components/Profile/MyPosts/MyPosts";
+
+const ADD_POST = 'ADD-POST';
+
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const messageReducer = (state: AllType, action: any) => {
-
+    if (action.type === ADD_POST) {
+        let newPost: PostType = {
+            id: 5,
+            message: state.messagePage.newPostText,
+            likesCount: 0
+        };
+        state.messagePage.posts.push(newPost);
+        state.messagePage.newPostText = '';
+    }  else if (action.type === UPDATE_NEW_POST_TEXT) {
+        state.messagePage.newPostText = action.newText;
+    }
     return state;
 }
 
--- src/redax/profile-reducer.tsx
+++ src/redax/profile-reducer.tsx
@@ -1,7 +1,22 @@
 import {AllType} from "../App";
+import {PostType} from "../components/Profile/MyPosts/MyPosts";
+import {MessageType} from "../components/Dialogs/Dialogs";
+
+const ADD_NEW_DIALOG = 'ADD-NEW-DIALOG';
+
+const UPDATE_NEW_DIALOG_TEXT = 'UPDATE-NEW-DIALOG-TEXT';
 
 const profileReducer = (state: AllType, action: any) => {
-
+    if (action.type === ADD_NEW_DIALOG) {
+        let newDialog: MessageType = {
+            id: 6,
+            message: state.profilePage.newDialogText
+        };
+        state.profilePage.messages.push(newDialog);
+        state.profilePage.newDialogText = '';
+    }  else if (action.type === UPDATE_NEW_DIALOG_TEXT) {
+        state.profilePage.newDialogText = action.newDialog;
+    }
     return state;
 }
 
